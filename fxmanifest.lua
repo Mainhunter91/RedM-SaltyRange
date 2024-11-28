4f1b51f1331e26412b9af992a75f8ea06f3619b1
M!R <165200384+Mainhunter91@users.noreply.github.com>
2024-11-28 13:13:17 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,10 @@
 fx_version 'cerulean'
 games { 'rdr3', 'gta5' }
-
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.' 
+
+author 'MIR91'
+description 'Show´s a Voicerange Circle around the Player'
+version '1.0.0'
 
 client_scripts {
     'main.lua'
